b7caa81338024cdb2fde3bf322a3f024e8cf4b22
manikpokhetra79 <manikpokhetra@gmail.com>
2022-02-01 17:11:28 +0530

    fix : convert code for rest api

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 "use strict";
 const AWS = require("aws-sdk");
+AWS.config.update({
+  region: "us-east-1",
+});
+const uploadPath = "/upload";
+const checkPath = "/check";
 const Jimp = require("jimp");
 const s3 = new AWS.S3();
 const { nanoid } = require("nanoid");
 let imageType;
 const bucket = process.env.Bucket;
-
-exports.handler = (event) => {
+const BucketUrl = process.env.BucketUrl;
+exports.handler = async function (event, context, callback) {
   let requestBody = JSON.parse(event.body);
+  let response;
+  switch (true) {
+    case event.httpMethod === "GET" && event.path === checkPath:
+      response = buildResponse(200, "Api Working");
+      break;
+    case event.httpMethod === "POST" && event.path === uploadPath:
+      response = await processRequest(requestBody, callback);
+      break;
+    default:
+      response = buildResponse(400, "Nope");
+  }
+  return response;
+};
+async function processRequest(requestBody, callback) {
   let base64Image = requestBody.photoUrl;
   let objectId = nanoid(10);
   let width = parseInt(requestBody.width);
@@ -17,9 +36,9 @@ exports.handler = (event) => {
   let imageExt = imageType.split("/")[1];
   let imageData = parts[1].split(",")[1];
   let objectKey = `${objectId}-${width}x${height}.${imageExt}`;
-  let encodedImage = Buffer.from(imageData, "base64");
+  let decodedImage = Buffer.from(imageData, "base64");
   let resp;
-  fetchImage(encodedImage)
+  await fetchImage(decodedImage)
     .then((image) => {
       return image.resize(width, height).getBufferAsync(imageType);
     })
@@ -30,21 +49,19 @@ exports.handler = (event) => {
       console.log(`Image ${objectKey} was uploaded and resized`);
       resp = buildResponse(200, {
         status: "success",
-        objectUrl: `${BucketUrl}/objectKey`,
+        objectUrl: `${BucketUrl}/${objectKey}`,
       });
     })
     .catch((error) => {
-      console.log(error);
       resp = buildResponse(500, {
         status: "failure",
         message: "Error while uploading image",
       });
     });
   return resp;
-};
-
-function uploadToS3(data, key) {
-  return s3
+}
+async function uploadToS3(data, key) {
+  return await s3
     .putObject({
       Bucket: bucket,
       Key: key,
@@ -59,10 +76,12 @@ function fetchImage(url) {
 }
 function buildResponse(statusCode, body) {
   return {
-    statusCode,
+    statusCode: statusCode,
     headers: {
-      "Access-Control-Allow-Origin": "*",
       "Content-Type": "application/json",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
     },
     body: JSON.stringify(body),
   };
